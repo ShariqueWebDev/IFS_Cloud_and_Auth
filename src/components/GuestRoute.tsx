5f26117af0816530deaaeba4054bfac36514ced2
sharique-aslam <shariquedev966@gmail.com>
2026-04-10 16:43:57 +0530

    fix login stale

--- src/components/GuestRoute.tsx
+++ src/components/GuestRoute.tsx
@@ -2,9 +2,11 @@ import { Navigate } from "react-router-dom";
 import { useGetAuthStatusQuery } from "../services/authApi";
 
 function GuestRoute({ children }: { children: React.ReactNode }) {
-  const { data, isLoading } = useGetAuthStatusQuery();
+  const { data, isLoading, isFetching } = useGetAuthStatusQuery(undefined, {
+    refetchOnMountOrArgChange: true,
+  });
 
-  if (isLoading)
+  if (isLoading || isFetching)
     return <p className="text-center mt-12 text-lg">Loading...</p>;
 
   if (data?.loggedIn) return <Navigate to="/dashboard" replace />;
